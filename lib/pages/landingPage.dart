bb9851e1c4976a9083126bf48ea9bee6d97995ca
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2024-12-11 03:52:55 +0600

    landing page initialized

--- lib/pages/landingPage.dart
+++ lib/pages/landingPage.dart
@@ -11,12 +11,44 @@ class _LandingPage extends State<LandingPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Image.asset("assets/landing_page.jpg"),
-            ElevatedButton(onPressed: (){}, child: Text("data2")),
+            Text("View product 360 degrees",
+              style: TextStyle(
+                color: Colors.brown,
+                fontSize: 22,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Text(
+              "You can see product with all angles, true, and convenient",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            ElevatedButton(
+                onPressed: (){},
+                child: Text(
+                    "data",
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor:Colors.brown,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)
+                )
+              ),
+            ),
           ],
         ),
       ),
